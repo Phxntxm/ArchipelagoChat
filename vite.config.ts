b54965689698f32a56d89c3567de4283850500d7
phantom <phxntxm1@gmail.com>
2026-08-28 13:23:30 -0700

    Attempt to correct the build for pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
+import tailwindcss from '@tailwindcss/vite'
 import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import { nitro } from "nitro/vite"
-
-const config = defineConfig(({command}) => {
-  const plugins = [
-      devtools(),
-      tailwindcss(),
-      tanstackStart(),
-      viteReact(),
-    ]
-  if (command === 'build') plugins.push(nitro())
-
-  return {
-    resolve: { tsconfigPaths: true },
-    plugins: plugins,
-  }
+export default defineConfig({
+  base: '/ArchipelagoChat/',
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart({
+      spa: {
+        enabled: true,
+        prerender: {
+          enabled: true,
+          crawlLinks: true,
+        },
+      },
+    }),
+  ],
 })
-
-export default config
